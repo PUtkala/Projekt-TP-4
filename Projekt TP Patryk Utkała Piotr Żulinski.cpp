1019c07e4d1ea1c0cedd5ebabae739c44d02ff72
PUtkala <51992192+PUtkala@users.noreply.github.com>
2019-06-21 21:08:17 +0200

    Add files via upload

--- Projekt TP Patryk Utkała Piotr Żulinski.cpp
+++ Projekt TP Patryk Utkała Piotr Żulinski.cpp
@@ -5,11 +5,21 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include <cmath>
-
+#include <vector>
+void moveLifter(int &lifterPositionx , int &lifterPositiony);
 int main()
 {
-	sf::RenderWindow okno(sf::VideoMode(320, 240), "Kurs SFML 2.0 - http://cpp0x.pl");
+	int lifterPositionx= 400;
+	int lifterPositiony= 50;
+	sf::RenderWindow okno(sf::VideoMode(1000, 600), "Dzwig");
 	sf::Clock stoper;
+	sf::Texture texture;
+	if (!texture.loadFromFile("dzwig.jpg"))
+	{
+		// error...
+	}
+	sf::Sprite sprite;
+	sprite.setTexture(texture);
 	while (okno.isOpen())
 	{
 		sf::Event event;
@@ -19,15 +29,43 @@ int main()
 				okno.close();
 
 		} //while
-		okno.clear();
+		okno.clear(sf::Color(255, 255, 255));
 
-		sf::CircleShape ksztalt(std::sin(stoper.getElapsedTime().asSeconds()) * okno.getSize().y / 8 + okno.getSize().y / 4);
-		ksztalt.setOrigin(sf::Vector2f(ksztalt.getRadius(), ksztalt.getRadius()));
-		ksztalt.setPosition(okno.getSize().x / 2.0f, okno.getSize().y / 2.0f);
-		ksztalt.setFillColor(sf::Color::Yellow);
-		okno.draw(ksztalt);
+		sprite.setPosition(250, 75);
+		okno.draw(sprite);
+		moveLifter(lifterPositionx, lifterPositiony);
+		sf::RectangleShape lifter(sf::Vector2f(5, lifterPositiony));
+		lifter.setPosition(lifterPositionx, 130);
+		lifter.setFillColor(sf::Color(0, 0, 0));
+		okno.draw(lifter);
+
+		sf::RectangleShape ground(sf::Vector2f(1000, 5));
+		ground.setPosition(0, 450); 
+		ground.setFillColor(sf::Color(0, 0, 0));
+		okno.draw(ground);
+		sf::Text text;
+		
 
 		okno.display();
 	} //while
 	return 0;
+}
+void moveLifter(int &lifterPositionx, int &lifterPositiony) {
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) && lifterPositionx < 630)
+	{
+		lifterPositionx += 1;
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A) && lifterPositionx > 400)
+	{
+		lifterPositionx -= 1;
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S) && lifterPositiony < 300)
+	{
+		lifterPositiony += 1;
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && lifterPositiony > 20)
+	{
+		lifterPositiony -= 1;
+	}
+
 }
